43672f39678ffd7a82883da6cd66c16f78a3758f
Austin Orr <AOrr@geosyntec.com>
2022-03-10 15:05:14 -0800

    patch bat file else statement

--- install_miniconda_for_user.bat
+++ install_miniconda_for_user.bat
@@ -4,8 +4,7 @@ if exist %UserProfile%\miniconda3 (
 	echo miniconda3 already exists for this user, exiting without modifying the system...
 	pause
 	EXIT /B
-)
-else (
+) else (
 	echo installing conda...
 	start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /S /D=%UserProfile%\miniconda3
 	echo installation complete.
